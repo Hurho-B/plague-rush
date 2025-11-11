80115ec471e91396916742ae817589d8256c02ea
cbutuceanu <161909929+cbutuceanu@users.noreply.github.com>
2025-11-11 11:31:25 -0500

    Simon Says Audio Done

--- Assets/Script/Puzzles/SimonSays/lightPuzzle.cs
+++ Assets/Script/Puzzles/SimonSays/lightPuzzle.cs
@@ -5,12 +5,21 @@ using UnityEngine.UI;
 
 public class lightPuzzle : MonoBehaviour
 {
+    [Header("Light Settings")]
     public Image[] lightColor;
     public int[] orderNb;
     private int maxNb = 4;
     private int nbTimedone;
     public GameObject blockClick;
+
+    [Header("Audio Settings")]
+    [SerializeField] AudioSource lightFlickerSound;
+    [SerializeField] AudioSource buttonPressSound;
+    [SerializeField] AudioSource correctCodeSound;
+    [SerializeField] AudioSource wrongCodeSound;
+
     private PuzzleManager puzzleManagerScript;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +27,18 @@ public class lightPuzzle : MonoBehaviour
         StartCoroutine(ShowColor());
     }
 
-
     IEnumerator ShowColor()
     {
         int i = Random.Range(0, lightColor.Length);
         orderNb[nbTimedone] = i;
         nbTimedone++;
+
+        // Play light flicker sound
+        if (lightFlickerSound != null)
+        {
+            lightFlickerSound.Play();
+        }
+
         Color currentColor = lightColor[i].color;
         Color startColor = currentColor;
         currentColor.a = 1f;
@@ -32,6 +47,7 @@ public class lightPuzzle : MonoBehaviour
         //Wait a bit 
         yield return new WaitForSeconds(0.7f);
         lightColor[i].color = startColor;
+
         if (nbTimedone == maxNb)
         {
             Debug.Log("Stop loop");
@@ -48,20 +64,40 @@ public class lightPuzzle : MonoBehaviour
 
     public void ClickLightButton(int whichButtton)
     {
+        // Play button press sound
+        if (buttonPressSound != null)
+        {
+            buttonPressSound.Play();
+        }
+
         if (whichButtton == orderNb[nbTimedone])
         {
             nbTimedone++;
             StartCoroutine(showClickColor(whichButtton));
+
             //if all correct you did it 
             if (nbTimedone == maxNb)
             {
                 Debug.Log("You Win");
+
+                // Play correct code sound
+                if (correctCodeSound != null)
+                {
+                    correctCodeSound.Play();
+                }
+
                 nbTimedone = 0;
                 puzzleManagerScript.FinishActivePuzzle();
             }
         }
         else
         {
+            // Play wrong code sound
+            if (wrongCodeSound != null)
+            {
+                wrongCodeSound.Play();
+            }
+
             //restart the loop 
             nbTimedone = 0;
             blockClick.SetActive(true);
@@ -79,4 +115,3 @@ public class lightPuzzle : MonoBehaviour
         lightColor[whichButton].color = startColor;
     }
 }
-
